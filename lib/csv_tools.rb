20e42acf3a05448c08127a3239cb27abeb0efd3c
Han Kessels <han.kessels@gmail.com>
2012-02-02 17:05:46 +0900

    Add iso-8859-1 to UTF-8 conversion on import
    - fixes UTF-8 incompatible character problems

--- lib/csv_tools.rb
+++ lib/csv_tools.rb
@@ -5,6 +5,7 @@ module CsvTools
   def self.prep_csv(file, first_field)
     s = file.tempfile.readline until s =~ /^#{first_field}([,;\t])/
     s += file.tempfile.read
+    s.force_encoding('iso-8859-1').encode!('utf-8')
     [s, $1]
   end
 end
