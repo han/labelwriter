b428cf72903a4d5066849baa772a6cf594d0067e
Han Kessels <han.kessels@gmail.com>
2012-02-06 01:55:18 +0900

    add inactive code for downloading pdf

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -5,6 +5,7 @@ class LabelsController < ApplicationController
       format.pdf do
         @ids = (params[:range] || '').split(',').map(&:to_i)
         @deliveries = Delivery.where(:id => @ids)
+        #response.headers['Content-Disposition'] = "attachment;filename=labels#{Time.now.strftime("%Y%m%d%_H%M%S")}.pdf"
         render 'index'
       end
     end
